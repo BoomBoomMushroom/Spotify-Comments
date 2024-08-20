a77fb327c1094ca9b1863d66c06cc15ed3e1268a
BoomBoomMushroom <dillioncat7@gmail.com>
2024-08-19 21:40:20 -0400

    Song upload?

--- consts.js
+++ consts.js
@@ -3,3 +3,4 @@ const codeChallengeCookieName = "codeChallenge"
 
 const CLIENT_ID = "5ee3aedb09a3447da667c5e06c276fb8"
 const scopes = 'user-read-private user-read-email user-read-currently-playing user-modify-playback-state user-read-playback-state'
+const backendApiUrl = "https://6ad817e4-d3b4-4e56-bdd1-88dc8590dddb-00-2wvpjzyuxvr1k.picard.replit.dev"
--- server/main.py
+++ server/main.py
@@ -1,15 +1,60 @@
 import flask
-import requests
 import json
 from flask import request
+from github import Github
 import os
-import base64
+import requests
+import threading
+import uuid
+import time
+
+token = os.environ['github_token']
+
+g = Github(token)
+user = g.get_user()
+apiRepo = user.get_repo("Spotify-Comments-API")
+
+# Database
+
+# per song
+comments = {}
+
+
+def syncGithubToDatabse():
+    comments = requests.get(
+        "https://raw.githubusercontent.com/BoomBoomMushroom/Spotify-Comments-API/main/comments.json"
+    ).json()
+
+
+def syncDatabaseToGithub():
+    commentsFileHolder = apiRepo.get_contents("comments.json", "main")
+    apiRepo.update_file(path=commentsFileHolder.path,
+                        message="Updated comments from db",
+                        content=json.dumps(comments, indent=4),
+                        sha=commentsFileHolder.sha)
+
+    print("Synced Database to Github")
+
+
+syncGithubToDatabse()
+
+
+def syncToGithub(firstPass=False):
+    # Rate limit is 5,000 requests per hour
+    # So lets call every 30 minutes
+    threading.Timer(30 * 60, syncToGithub).start()
+
+    if (firstPass):
+        print("First pass! Don't sync")
+        return
+    print("Sync called!")
+    syncDatabaseToGithub()
+
+
+# Kickstart the process
+syncToGithub(True)
 
 app = flask.Flask(__name__)
-
-bothSpotifyStuff = os.environ['SPOTIFY_ID'] + ":" + os.environ['SPOTIFY_SECRET']
-b64SpotifyAuth = base64.b64encode(
-    bothSpotifyStuff.encode('utf-8')).decode('utf-8')
 
 
 def responseMake(r):
@@ -24,7 +69,86 @@ def responseMake(r):
 
 @app.route("/")
 def home():
-    return responseMake("Backend for Spotify Comments"), 20
+    return responseMake("Backend for Spotify Comments"), 200
+
+
+"""
+# Comment Example
+[
+    {
+        "UUID": "UUID_DEMO",
+        "SongTrackId": "SongTrackId",
+        "user": {
+            "username": "John Doe",
+            "id": "USER_ID_DEMO",
+            "profileURL": "https://spotify.com",
+            "pfpURL": "https://i.scdn.co/image/ab6775700000ee85f4b8be4cfa24c9e067201f72"
+        },
+        "commentMeta": {
+            "message": "Hello world!",
+            "likes": [],
+            "dislikes": [],
+            "replyCount": 0,
+            "replyingTo": null,
+            "isEdited": false,
+            "postTime": 0, // in seconds since epoch
+        }
+    }
+]
+"""
+
+
+@app.route("/comments")
+def getComments():
+    try:
+        songId = str(request.args.get('song_id'))
+    except:
+        flask.abort(400)
+        #return 400
+
+    result = []
+
+    return responseMake(result), 200
+
+
+@app.route("/postcomment")
+def getComments():
+    try:
+        songId = str(request.args.get('song_id'))
+        message = str(request.args.get('message'))
+        userId = str(request.args.get('user_id'))
+        username = str(request.args.get('username'))
+        pfpUrl = str(request.args.get('pfp_url'))
+    except:
+        flask.abort(400)
+        #return 400
+
+    newComment = {
+        "UUID": str(uuid.uuid4()),
+        "SongTrackId": songId,
+        "user": {
+            "username": username,
+            "id": userId,
+            "profileURL": "https://open.spotify.com/user/" + userId,
+            "pfpURL": pfpUrl
+        },
+        "commentMeta": {
+            "message": message,
+            "likes": [],
+            "dislikes": [],
+            "replyCount": 0,
+            "replyingTo": None,
+            "isEdited": False,
+            "postTime": time.time(),
+        }
+    }
+
+    if songId not in comments:
+        comments[songId] = []
+
+    comments[songId].append(newComment)
+
+    return responseMake(""), 200
 
 
 app.run(host="0.0.0.0", port=7777)
